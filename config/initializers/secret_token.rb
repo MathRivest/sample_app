a399cf8c166ca6e5e1ee6a72bbaf03fb15a11094
MathRivest <rivest.mathieu@gmail.com>
2012-12-29 12:06:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e3e5900bc527cb58029478e4620c544feeb210dc144be3471b0cde41aba45636f7f315e3210eb3062deac39a8c5ab2b6a39bee3e714e325c3d91378bdc6d7233'
